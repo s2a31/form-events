73020d38bc9298ce3e59904090974ae7f3fe0cdd
s2a31 <baloghszabolcs43@gmail.com>
2022-09-06 15:24:32 +0300

    Add 'Grocery-List' example.

---
+++ Grocery-List/app.js
@@ -0,0 +1,17 @@
+const form = document.querySelector('form');
+const list = document.querySelector('#list');
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const productInput = form.elements.product;
+  const qtyInput = form.elements.qty;
+  addProduct(productInput.value, qtyInput.value);
+  productInput.value = '';
+  qtyInput.value = '';
+});
+
+const addProduct = (product, qty) => {
+  const newItem = document.createElement('li');
+  newItem.innerText = `${qty} ${product}`;
+  list.appendChild(newItem);
+};
